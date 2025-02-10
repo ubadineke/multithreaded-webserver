d0d0c5761027323549ff2029d9a70afc60f5a1dd
ubadineke <ubadinekeprince@gmail.com>
2025-02-11 00:34:23 +0100

    graceful shutdown and cleanup

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,7 @@ fn main() {
 
     for stream in listener.incoming(){
         let stream = stream.unwrap();
-        // handle_connection(stream);
-        // thread::spawn(|| {
-        //     handle_connection(stream);
-        // });
+       
         pool.execute(|| {
             handle_connection(stream);
         });
